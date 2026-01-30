e425f5730c08079053cd13b61f16ce3755aacb22
pipsyclone <apiprs24@gmail.com>
2026-01-31 03:17:59 +0700

    feat: add health check endpoint and update healthcheck path in railway configuration

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\IndexController::class, 'index'])->name('index');
+
+// routes/web.php
+Route::get('/health', function () {
+    return response()->json(['status' => 'healthy']);
+});
