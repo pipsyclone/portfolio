b1d61fe14ccceedf333f8d8b22a929bbd1d5fd63
pipsyclone <apiprs24@gmail.com>
2026-06-21 11:28:59 +0700

    feat: edit db backup command

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('db:backup-backup')->weeklyOn(0, '23:55'); // Menjadwalkan backup database setiap minggu pada pukul 23:55
+// Schedule::command('db:me')->weeklyOn(0, '23:55'); // Menjadwalkan backup database setiap minggu pada pukul 23:55
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
