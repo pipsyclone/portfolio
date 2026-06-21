bb7a4d80eedef42266a35a667286c3d1bfab9d7d
pipsyclone <apiprs24@gmail.com>
2026-06-21 10:44:41 +0700

    feat: update timezone app

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('db:backup')->weekly()->mondays()->at('00:00'); // Menjadwalkan backup database setiap minggu pada pukul 00:00
+Schedule::command('db:backup'); // Menjadwalkan backup database setiap minggu pada pukul 00:00
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
