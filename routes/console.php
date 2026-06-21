804efc04517d56b23e159ae1b0b3d33bdb75090e
pipsyclone <apiprs24@gmail.com>
2026-06-21 10:58:13 +0700

    feat: update scheduler backup database every week

--- routes/console.php
+++ routes/console.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('db:backup'); // Menjadwalkan backup database setiap minggu pada pukul 00:00
+Schedule::command('db:backup')->weeklyOn(0, '23:55'); // Menjadwalkan backup database setiap minggu pada pukul 23:55
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
