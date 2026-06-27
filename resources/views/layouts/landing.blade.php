f5d466ae4eb206fd8eabfcdd7ea6c844e0a8b64f
pipsyclone <apiprs24@gmail.com>
2026-06-27 20:48:27 +0700

    fix: bug theme darkmode not saved

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -354,6 +354,13 @@
         }
 
         document.addEventListener('livewire:navigated', () => {
+            // Re-apply theme since livewire navigate might replace html class attribute
+            if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+                document.documentElement.classList.add('dark');
+            } else {
+                document.documentElement.classList.remove('dark');
+            }
+
             initAOS();
             initVanta();
             syncThemeIcon();
